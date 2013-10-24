0d507e333d9bcaf2332e8f77b5da9bd15b9e9ed8
Jonathan Kupcho <jonathan.kupcho@gmail.com>
2013-10-23 23:57:06 -0500

    updated paddle movement

--- WebContent/pong/objects/entities/paddle.js
+++ WebContent/pong/objects/entities/paddle.js
@@ -2,8 +2,16 @@ var Game = Game || {};
 
 Game.Paddle = function(name,color,x,y,w,h)
 {
+	this.y = y;
+	this.x = x;
 	NoobJS.ObjectGraph.Node.call(this,name);
 	this.color = color;
-	this.box2dObj = Game.noobJS.noobBox2d.createBoxfunction(x, y, w, h, true);
+	this.box2dObj = Game.noobJS.noobBox2d.createBoxfunction(this.x, this.y, w, h, true);
+
+	this.updatePaddle = function() {
+		remove = this.box2dObj;
+		this.box2dObj = Game.noobJS.noobBox2d.createBoxfunction(this.x, this.y, w, h, true);
+		Game.noobJS.noobBox2d.world.DestroyBody(remove);
+	};
 };
 

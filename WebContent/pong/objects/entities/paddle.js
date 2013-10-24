1a5f8d4da64f870d681ef7b61f3692cf220583de
Jonathan Kupcho <jonathan.kupcho@gmail.com>
2013-10-24 17:38:43 -0500

    Updated paddle logic to move the position
    Adds static flag, then removes (if it wasn't already static, current
    implementation the paddle is already static)

--- WebContent/pong/objects/entities/paddle.js
+++ WebContent/pong/objects/entities/paddle.js
@@ -9,9 +9,15 @@ Game.Paddle = function(name,color,x,y,w,h)
 	this.box2dObj = Game.noobJS.noobBox2d.createBoxfunction(this.x, this.y, w, h, true);
 
 	this.updatePaddle = function() {
-		remove = this.box2dObj;
-		this.box2dObj = Game.noobJS.noobBox2d.createBoxfunction(this.x, this.y, w, h, true);
-		Game.noobJS.noobBox2d.world.DestroyBody(remove);
+		var flagsBefore = this.m_flags;
+		// Activate static flag if it isn't already
+		this.m_flags = this.m_flags | b2Body.e_staticFlag;
+		this.box2dObj.m_position.y = this.y;
+		this.box2dObj.SynchronizeShapes()
+		if(flagsBefore !== this.m_flags) {
+			// Remove static flag
+			this.m_flags = this.m_flags & ~b2Body.e_staticFlag;
+		}
 	};
 };
 
